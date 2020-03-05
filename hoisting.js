0989ef8008ba1dcb079591c829d8ff686971ff2f
MJU <marie.ju2012@outlook.com>
2020-03-05 22:06:20 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,16 @@
 function callMe() {
-  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  // var lyric = "maybe";
+  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  console.log(thisIsCrazy)
+  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
