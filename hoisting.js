7c81b8db25f5e73315cb08847123676472de9593
MJU <marie.ju2012@outlook.com>
2020-03-05 22:05:32 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,7 +11,7 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  console.log(thisIsCrazy();)
+  console.log(thisIsCrazy)
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
